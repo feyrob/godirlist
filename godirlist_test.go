354bc8c01bde8af09a337cf608f692a284fdc229
feyrob <feyrob@gmail.com>
2020-12-04 08:56:25 +0000

    make it faster still

--- godirlist_test.go
+++ godirlist_test.go
@@ -2,6 +2,7 @@ package godirlist
 
 import (
 	"fmt"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -9,41 +10,39 @@ import (
 func Test_1(t *testing.T) {
 	fmt.Println("hi")
 	test_abspath := `C:\Program Files`
-	//test_abspath = `C:\`
+	test_abspath = `C:\`
 	worker_count := 64
 	paths := []string{
 		test_abspath,
 	}
 	startTime := time.Now()
-	fsitem_chan := make(chan FsitemInfo)
+
+	file_count := int64(0)
+	directory_count := int64(0)
+	other_count := int64(0)
+	total_size := int64(0)
 
 	result_handler := func(some_results []FsitemInfo) {
-		for _, result := range some_results {
-			_ = result
-			fsitem_chan <- result
+		for _, fsitem := range some_results {
+
+			//fsitem_chan <- fsitem
+			// do stuff with found files ...
+			if fsitem.Fi.Mode().IsRegular() {
+				s := fsitem.Fi.Size()
+				atomic.AddInt64(&total_size, s)
+				atomic.AddInt64(&file_count, 1)
+
+			} else if fsitem.Fi.IsDir() {
+				atomic.AddInt64(&directory_count, 1)
+
+			} else {
+				atomic.AddInt64(&other_count, 1)
+			}
 		}
 	}
 
-	go func() {
-		GenerateFsitemInfos(paths, result_handler, worker_count)
-		close(fsitem_chan)
-	}()
-	file_count := 0
-	directory_count := 0
-	other_count := 0
-	total_size := int64(0)
-	for fsitem := range fsitem_chan {
-		// do stuff with found files ...
-		if fsitem.Fi.Mode().IsRegular() {
-			s := fsitem.Fi.Size()
-			total_size += s
-			file_count++
-		} else if fsitem.Fi.IsDir() {
-			directory_count++
-		} else {
-			other_count++
-		}
-	}
+	GenerateFsitemInfos(paths, result_handler, worker_count)
+
 	duration := time.Since(startTime)
 	fmt.Println("wait:", duration, "bytes:", total_size, "files:", file_count, "directories:", directory_count, "other:", other_count)
 }
